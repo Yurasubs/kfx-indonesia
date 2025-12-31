b56e6669da2ffd927c4d70e25de9adc50cab519e
Aruh1 <rizkypraschool@gmail.com>
2025-12-31 19:09:22 +0700

    added watayuri

---
+++ jsso/watayuri-op/burn-in.js
@@ -0,0 +1,12 @@
+const renderer = new SubtitlesOctopus({
+    video: document.querySelector('video'),
+    renderMode: 'lossy',
+    subUrl: '../subtitles/OPEDYuri.ass',
+    workerUrl: '../subtitles-octopus-worker.js',
+    fallbackFont: '../fonts/default.woff2',
+    fonts: [
+        '../fonts/00209.TTF',
+        '../fonts/JANIHB.ttf'
+    ],
+    // timeOffset: -0.041
+})
