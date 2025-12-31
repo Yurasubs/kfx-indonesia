3dabb8c4d6f6f7e0b2769f87aaa6088c2289bdc4
Aruh1 <rizkypraschool@gmail.com>
2025-12-31 19:36:03 +0700

    added makeine

---
+++ arsip/makeine-op/burn-in.js
@@ -0,0 +1,11 @@
+const renderer = new SubtitlesOctopus({
+    video: document.querySelector('video'),
+    renderMode: 'lossy',
+    subUrl: '../subtitles/makeineed.ass',
+    workerUrl: '../subtitles-octopus-worker.js',
+    fallbackFont: '../fonts/default.woff2',
+    fonts: [
+        '../fonts/Futura ND Medium.ttf',
+    ],
+    // timeOffset: -0.041
+})
