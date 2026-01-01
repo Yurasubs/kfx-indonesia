c4d127310bddbccbae80a3e2a2e9a1941968a412
Aruh1 <rizkypraschool@gmail.com>
2026-01-01 14:55:41 +0700

    feat: Add JASSUB burn-in example scripts with different subtitle and font configurations.

--- jassub/makeine-op/burn-in.js
+++ jassub/makeine-op/burn-in.js
@@ -2,8 +2,10 @@ const renderer = new JASSUB({
     video: document.querySelector('video'),
     renderMode: 'lossy',
     subUrl: '../subtitles/makeineed.ass',
-    workerUrl: '../subtitles-octopus-worker.js',
-    fallbackFont: '../fonts/default.woff2',
+    workerUrl: '../assets/jassub-worker.js',
+    availableFonts: {
+        'liberation sans': '../../fonts/default.woff2'
+    },
     fonts: [
         '../fonts/Futura ND Medium.ttf',
     ],
